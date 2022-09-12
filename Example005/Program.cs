018431422e7c19afe5c45e582456b3f857bfc3ff
Nata <muravevantsh@mail.ru>
2022-09-13 01:24:09 +0300

    Example005

---
+++ Example005/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу вычисления функции Аккермана
+
+int FunctionAkk(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (m > 0 && n == 0) return FunctionAkk(m - 1, 1);
+    else return FunctionAkk(m - 1, FunctionAkk(m, n - 1));
+}
+Console.WriteLine(FunctionAkk(3, 5));
