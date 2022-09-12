3a6abbc1e5f37d3025e8a81c312790376698d86e
Nata <muravevantsh@mail.ru>
2022-09-13 01:02:28 +0300

    Example001

---
+++ Example001/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать натуральные числа от N до 1, N задано
+string result = string.Empty;
+string Number(int n)
+{
+    result += n + " ";
+    if (n > 1) Number(n - 1);
+    return result;
+}
+Console.WriteLine(Number(10));
