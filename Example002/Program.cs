4c545134ae55792c7eb035f990bb827c1afd0da9
Nata <muravevantsh@mail.ru>
2022-09-13 01:09:36 +0300

    Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,7 @@
+﻿// Показать натуральные числа от M до N, N и M заданы
+string NumbersRec (int M, int N)
+{
+    if(M<=N) return $"{M}" + NumbersRec (M+1, N);
+    else return String.Empty;
+}
+Console.WriteLine(NumbersRec (1,12));
