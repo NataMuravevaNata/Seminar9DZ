9cb6f571e7c4d05a35146abf149f2e8e487fa121
Nata <muravevantsh@mail.ru>
2022-09-13 01:15:17 +0300

    Example003

---
+++ Example003/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+int temp = 0;
+int SummNumber(int m, int n)
+{
+    if (m < n) SummNumber(m, n - 1);
+    temp += n;
+    return temp;
+}
+Console.WriteLine(SummNumber(1, 100));
