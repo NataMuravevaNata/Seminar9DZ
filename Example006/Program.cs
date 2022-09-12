26b8c7eb73caa865f24819a72204013af4f8101a
Nata <muravevantsh@mail.ru>
2022-09-13 01:36:09 +0300

    Example006

---
+++ Example006/Program.cs
@@ -0,0 +1,9 @@
+﻿// Написать программу возведения числа А в целую степень B
+
+int PowerRec (int A, int B);
+{
+    if (B == 0) return 1;
+    else return PowerRec (A, B-1)*A;
+}
+Console.WriteLine(PowerRec(2,8));
+
